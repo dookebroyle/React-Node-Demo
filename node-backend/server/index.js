f3ec99896e120f056404e8bb22ae114814ebe529
Doyle <bld1@otpco.com>
2022-06-16 15:35:12 -0500

    first commit

---
+++ node-backend/server/index.js
@@ -0,0 +1,67 @@
+import express from 'express'
+const app = express();
+const PORT = process.env.PORT || 3001;
+import fetch from "node-fetch";
+import bodyParser from "body-parser"
+import https from "https"
+const router = new express.Router()
+
+
+
+const agent = new https.Agent({
+  rejectUnauthorized: false,
+  headers : { 
+    'Content-Type': 'application/json',
+    'Accept': 'application/json'
+   }
+})
+
+
+
+
+
+// Handle GET requests to /api route
+app.get("/api", async (req, res) => {
+  
+  fetch('https://catfact.ninja/fact', {agent})
+  .then(response => {
+    //console.log(response);
+    return response.json();
+  }).then(data => {
+    // Work with JSON data here
+    console.log({data});
+    res.json({data})
+  }).catch(err => {
+    // Do something for an error here
+    console.log("Error Reading data " + err);
+  });
+
+
+
+
+  // fetch('https://catfact.ninja/fact', {agent})
+  // .then(res => { res.json();})
+  // .then(json => {
+  //   // Work with JSON data here
+  //   console.log({json});
+  //   res.send({json})
+  // }).catch(err => {
+  //   // Do something for an error here
+  //   console.log("Error Reading data " + err);
+  // });
+})
+
+
+
+  
+// // All other GET requests not handled before will return our React app
+// app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+//   });
+
+  app.listen(PORT, () => {
+    console.log(`Server listening on ${PORT}`);
+  });
+
+
+  
